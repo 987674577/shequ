4cd69d153080fe784462ea19f3e963077375ef56
liujiyu <987674577@qq.com>
2019-06-19 17:55:20 +0800

    1.0

--- src/main/java/com/wzcdc/shequ/controller/IndexController.java
+++ src/main/java/com/wzcdc/shequ/controller/IndexController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
+ * 目录控制器，将url请求转向目标html
+ *
  * @author 刘孝丰
  * @date 2019-06-17 11:03
  */
@@ -25,5 +27,28 @@ public class IndexController {
     @GetMapping("/zz/add")
     public String getZzAdd() {
         return "zz/add";
+    }
+
+
+    @GetMapping("/sf")
+    public String getSfMain() {
+        return "sf/main";
+    }
+
+
+    @GetMapping("/sf/add")
+    public String getSfAdd() {
+        return "sf/add";
+    }
+
+
+    /**
+     * 测试用，第一次随访记录页面
+     *
+     * @return
+     */
+    @GetMapping("/sf/add1")
+    public String getSfAdd1() {
+        return "sf/first-add";
     }
 }
